e908ee908b492bedabd0495ac3304ec2c67fefd7
ranayave <59626565+ranayave@users.noreply.github.com>
2020-03-28 20:49:15 -0700

    added new modifications to test

--- Test.java
+++ Test.java
@@ -1,8 +1,15 @@
+import java.util.*;
 class Test{
-  public static void main(String[] args){
-    for(int i = 0; i < args.length ; i++){
-      System.out.print(args[i]);
-      //do some cool stuff
+    public static void main(String[] args){
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter name");
+        String name = myObj.nextLine();
+        System.out.println("enter age");
+        int age = myObj.nextInt();
+        if(age<18){
+            System.out.print("You need to be older than 18 to watch");
+        }else{
+            System.out.println("Welcome, "+name + "you're " + age );
+        }
     }
-  }
 }
