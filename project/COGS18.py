2eb7dadcd42e091caa88bf86fe04da2e4b327309
ranayave <59626565+ranayave@users.noreply.github.com>
2020-05-30 19:48:08 -0700

    Second commit, did some modifications

--- project/COGS18.py
+++ project/COGS18.py
@@ -1,36 +1,56 @@
 class UtilityBelt:
-    def __init__(self,operation):
-        self.operation = operation
+    def __init__(self,reason):
+        self.reason = reason
     def chat_with_me(self):
         return self
-    def calculator(self,operation):
-        if(self.operation == '+'):
+    def calculator(self):
+        operation = input('Select an operation ie.(*,+,/,^,%,Avr): ')
+        if(operation == '+'):
             x = input('Enter number to add: ')
             y = input('Enter second number to add: ')
             a = int(x) + int(y)
             print(a)
-        elif(self.operation == '-'):
+        elif(operation == '-'):
             ###promt to choose what numbers to subtract
             x = input('Enter number to subtract: ')
             y = input('Enter number to subtract: ')
-            a = x - y
-            return a 
-        elif(self.operation == '*'):
+            a = int(x) - int(y)
+            print(a) 
+        elif(operation == '*'):
             ###promt to choose what numbers to multiply
-            a = x*y
-            return a 
-        elif(self.operation == '/'):
+            x = input('Enter number to multiply: ')
+            y = input('Enter 2nd number to multiply: ')
+            a = int(x)*int(y)
+            print(a) 
+        elif(operation == '/'):
             ###promt to choose what numbers to divide
-            a = x/y
-            return a
-        elif(self.operation == '%'):
+            x = input('Enter number to divide(numerator): ')
+            y = input('Enter number to divide(denominator): ')
+            a = int(x)/int(y)
+            print(a)
+        elif(operation == '%'):
             ###promt to choose what numbers to get the remainder for
-            a = x%y
-            return a
-        elif(self.operation == '^'):
+            x = input('Enter number to find its remainder: ')
+            y = input('Enter remaider value: ')
+            a = int(x)%int(y)
+            print(a)
+        elif(operation == '^'):
             ###promt to choose what numbers to power
-            a = x**y
-            return a
+            x = input('Enter number to square: ')
+            y = input('Enter exponent: ')
+            a = int(x)**int(y)
+            print(a)
+        elif(operation == 'Avr'):
+            x = int(input('Enter amount of values in list: '))
+            counter = 0
+            summation = 0
+            for i in range(0,x):
+                average = int(input('Enter number: '))
+                summation += average 
+            print('The average of entered numbers is: ' + str(summation/x))
+        else:
+            print('Not part of the options')
     def curr_time(self):
         return self
-
+Attempt = UtilityBelt('^')
+Attempt.calculator()
